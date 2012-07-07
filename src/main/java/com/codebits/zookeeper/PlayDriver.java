356edf90487637932f83d03f91538f5d6b54473c
David Medinets <david.medinets@gmail.com>
2012-07-07 13:41:20 -0400

    some code to play with zookeeper.

---
+++ src/main/java/com/codebits/zookeeper/PlayDriver.java
@@ -0,0 +1,41 @@
+package com.codebits.zookeeper;
+
+import java.util.List;
+
+import org.apache.accumulo.server.client.HdfsZooInstance;
+import org.apache.accumulo.server.zookeeper.IZooReaderWriter;
+import org.apache.accumulo.server.zookeeper.ZooReaderWriter;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+public class PlayDriver {
+
+	public void list(ZooKeeper zk, String path) {
+		try {
+			Stat stat = zk.exists(path, null);
+			if (stat != null) {
+				String value = new String(zk.getData(path, null, null));
+				System.out.println(path + " -> " + value);
+				int numChildren = stat.getNumChildren();
+				if (numChildren == 0) {
+				} else {
+					List<String> children = zk.getChildren(path, null);
+					for (String child : children) {
+						String newPath = "/".equals(path) ? path + child : path + "/" + child;
+						list(zk, newPath);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		ZooKeeper zk = ZooReaderWriter.getInstance().getZooKeeper();
+	    String iid = HdfsZooInstance.getInstance().getInstanceID();
+		PlayDriver driver = new PlayDriver();
+		driver.list(zk, "/accumulo");
+	}
+
+}
